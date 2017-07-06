4fb8e16241e29ef91f88a8002fd77c4415908483
David Vollmar <vollmad@bgsu.edu>
2017-07-06 10:04:21 -0400

    Setup workpace for projects using HelloWorld example

---
+++ David_Vollmar_Code/workspace/HelloWorld/src/Hello/HelloWorld.java
@@ -0,0 +1,18 @@
+package Hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		//syso ctrl -space
+		//main ctrl -space
+		//inline comment
+		
+		/*
+		 * Ctrl - D to delete a line
+		 * multi line comment
+		 */
+	}
+
+}
