52bbf67966cee1c5009190e9e07ad321a8649e73
David Vollmar <vollmad@bgsu.edu>
2017-07-19 20:18:11 -0400

    updated an .gitignore file for new directory and fixed the example maven connection project that magically disappeared.

---
+++ David_Vollmar_SqlCode/workspace/JDBCFullExample/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,18 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/* Final utility class to obtain connections in a modular way */
+public final class ConnectionUtil {
+	
+	/* Get connection to JDBC */
+	public static Connection getConnection() throws SQLException {
+		String url = "usfdbjava.cvywdbgy1ui6.us-east-2.rds.amazonaws.com:1521:ORCL";
+		String username = "login_test_in";
+		String password = "p4ssw0rd";
+		
+		return DriverManager.getConnection(url, username, password);
+	}
+}
