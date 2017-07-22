c3cef23c4fa7b98af3cc590fd88588e5c776c92f
Peter Alagna <peter.alagna@revature.com>
2017-07-21 21:20:36 -0400

    Added FrontController full example with comments and log4j fixed.

---
+++ servlets/examples/FrontController/src/main/java/com/revature/util/FinalUtil.java
@@ -0,0 +1,6 @@
+package com.revature.util;
+
+/* Utility class with just static and final variables used across the project */
+public final class FinalUtil {
+	public static final String HTTP_GET = "GET";
+}
