e317cffd8d4da932e362f3514e30bce1be6544cb
David Vollmar <vollmad@bgsu.edu>
2017-07-22 02:08:21 -0400

    update example code for Servlets and maven Servlet example

--- David_Vollmar_SqlCode/workspace/1707_Java_Servlets/src/com/revature/servlets/HelloWorld.java
+++ David_Vollmar_SqlCode/workspace/1707_Java_Servlets/src/com/revature/servlets/HelloWorld.java
@@ -3,55 +3,40 @@ package com.revature.servlets;
 import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
-@WebServlet("/HelloWorld")
+//@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloWorld() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		System.out.println("init");
+		System.out.println("HelloWorld - init");
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
 		System.out.println("destroy");
 	}
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("service");
-	}
+//	@Override
+//	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+//		super.service(arg0, arg1);
+//	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    @Override
+    public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	System.out.println("service");
+    	super.service(req, resp);
+    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("doGet");
 	}
